b88099703bf4ae4a090a51207c960af062cd743b
Israel Lot <israel.lot@gmail.com>
2015-04-23 21:03:14 +0800

    Proper credits for Jeroen Domburg <jeroen@spritesmods.com>

--- app/http/http_helper.c
+++ app/http/http_helper.c
@@ -1,9 +1,10 @@
 /*
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * Israel Lot <me@israellot.com> wrote this file. As long as you retain 
- * this notice you can do whatever you want with this stuff. If we meet some day, 
- * and you think this stuff is worth it, you can buy me a beer in return. 
+ * Israel Lot <me@israellot.com> and Jeroen Domburg <jeroen@spritesmods.com> 
+ * wrote this file. As long as you retain this notice you can do whatever you 
+ * want with this stuff. If we meet some day, and you think this stuff is 
+ * worth it, you can buy us a beer in return. 
  * ----------------------------------------------------------------------------
  */
 
